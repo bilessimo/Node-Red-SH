2bf08c1543b73c02ac0781fb5dfcc34866b27e52
bilessimo <44207592+bilessimo@users.noreply.github.com>
2020-07-11 15:42:16 -0300

    Update with VSC (PlatformIO) friendly version

--- garden/ESP-01/SH-Esp-01-MQTT_Bridge.ino
+++ garden/ESP-01/SH-Esp-01-MQTT_Bridge.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h> // Only needed if you're using VSCode.
+
 /*
 The ESP-01 connected a Arduino Nano (Rev. 3) to the WiFi Network and act like a bridge with the MQTT Server running on a Raspeberry Pi, working both ways.
 */
@@ -7,15 +9,20 @@ The ESP-01 connected a Arduino Nano (Rev. 3) to the WiFi Network and act like a 
 
 const char* ssid         = "SSID";// WiFi Network SSID.
 const char* password     = "password";      // WiFi Network password.
-const char* mqttServer   = "192.168.0.X";   // MQTT Server's IP Adress on WLAN. 
+const char* mqttServer   = "192.168.0.13";   // MQTT Server's IP Adress on WLAN (I'm running it on an fixed IP Raspberry Pi Model 3). 
 const int   mqttPort     = 1883;            // Port used by the MQTT Server.
-
 
 #define PUB_ARDUINO_STATUS "sh/garden/status"   // Topic on the MQTT Server used to receive the status message sent by the Arduino Nano.
 #define SUB_ARDUINO_ACTION "sh/garden/action"   // Topic on the MQTT Server used to send messages to the Ardunino Nano.
 
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
+
+// This block is only necessary if you're using VSCode. If you're using the Arduino IDE, it does this for you.
+void checkForMessage();
+void initWifiStation();
+void initMQTTClient();
+void PubSubCallback(char* topic, byte* payload, unsigned int messageLength);
 
 void setup() {
   
@@ -37,12 +44,12 @@ void checkForMessage() {
   String incomingString="";
 
   while (Serial.available()){
-    
-    incomingString=Serial.readString();
-    char stringConvertedToChar[256];  // Create a char array that will be used by mqttClient, since it don't accept a String.
-    incomingString.toCharArray(stringConvertedToChar, 256); // Convert the String in the cahr array created above.
-    mqttClient.publish(PUB_ARDUINO_STATUS, stringConvertedToChar);  // Publish the message.
-    
+	
+	incomingString=Serial.readString();
+	char stringConvertedToChar[256];  // Create a char array that will be used by mqttClient, since it don't accept a String.
+	incomingString.toCharArray(stringConvertedToChar, 256); // Convert the String in the cahr array created above.
+	mqttClient.publish(PUB_ARDUINO_STATUS, stringConvertedToChar);  // Publish the message.
+	
   }
 }
 
@@ -51,13 +58,13 @@ void initWifiStation() {
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);    
 
-//  Serial.print("\nConnecting to WiFi");  // Uncomment to debug to serial monitor
+ Serial.print("\nConnecting to WiFi");  // Uncomment to debug to serial monitor
   
   while (WiFi.status() != WL_CONNECTED) {
-     delay(1000);        
-//     Serial.print(".");  // Uncomment to debug to serial monitor
+	 delay(1000);        
+    Serial.print(".");  // Uncomment to debug to serial monitor
   }
-//  Serial.println(String("\nConnected to the WiFi network (") + ssid + ")" );  // Uncomment to debug to serial monitor
+ Serial.println(String("\nConnected to the WiFi network (") + ssid + ")" );  // Uncomment to debug to serial monitor
 }
  
 void initMQTTClient() {
@@ -67,19 +74,19 @@ void initMQTTClient() {
   mqttClient.setCallback(PubSubCallback);
   
   while (!mqttClient.connected()) {
-    
-//    Serial.println(String("Connecting to MQTT (") + mqttServer + ")..."); //Uncomment to debug to serial monitor
-    if (mqttClient.connect("ESP8266Client")) {
-//      Serial.println("MQTT client connected");  // Uncomment to debug to serial monitor
-    } else {
-//      Serial.print("\nFailed with state "); // Uncomment to debug to serial monitor
-//      Serial.println(mqttClient.state()); // Uncomment to debug to serial monitor
-            
-        if (WiFi.status() != WL_CONNECTED) {
-           initWifiStation();
-        }
-        delay(2000);
-    }
+	
+   Serial.println(String("Connecting to MQTT (") + mqttServer + ")..."); //Uncomment to debug to serial monitor
+	if (mqttClient.connect("ESP8266Client")) {
+     Serial.println("MQTT client connected");  // Uncomment to debug to serial monitor
+	} else {
+     Serial.print("\nFailed with state "); // Uncomment to debug to serial monitor
+     Serial.println(mqttClient.state()); // Uncomment to debug to serial monitor
+			
+		if (WiFi.status() != WL_CONNECTED) {
+		   initWifiStation();
+		}
+		delay(2000);
+	}
   }
 
   // Declare Pub/Sub topics
@@ -87,10 +94,10 @@ void initMQTTClient() {
   mqttClient.subscribe(SUB_ARDUINO_ACTION);
 }
 
-void PubSubCallback(char* topic, byte* payload, unsigned int length) {
+void PubSubCallback(char* topic, byte* payload, unsigned int messageLength) {
   String strPayload = "";
-  for (int i = 0; i < length; i++) {
-      Serial.print((char)payload[i]);
-      strPayload += (char)payload[i];
+  for (int i = 0; i < messageLength; i++) {
+	  Serial.print((char)payload[i]);
+	  strPayload += (char)payload[i];
   }
 }
